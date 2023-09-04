34ea3123912bec3c97768ab23b23260c853d90d1
Samuel Díaz <112455018+PanfletaGG@users.noreply.github.com>
2023-09-04 11:57:29 -0500

    Add files via upload

---
+++ jsXD/Main.js
@@ -0,0 +1,13 @@
+function suma(){
+    let a = 0;
+    let b = 0;
+
+
+a = parseInt(prompt("Por favor pon el primer digito: "));
+b = parseInt(prompt("Por favor pon el primer digito: "));
+
+suma = a + b;
+
+alert("la suma de los digitos es: " + suma)
+
+}
